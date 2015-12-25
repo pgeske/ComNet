438102cde23f530889cca354b33a13ab1e29b6cb
Philip Geske <pgeske@Philips-MacBook-Pro.local>
2015-12-24 21:52:24 -0500

    Visual changes.

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,24 @@
 var app = angular.module('ComNet', ['ngRoute', 'ngStorage']);
 
 app.run(function ($rootScope, $location, $route, AuthenticationService) {
+    //========================
+    // Root Scope Dual Binding
+    //========================
+    $rootScope.pageStatus = {
+        'views/chat.html': {class: ''}
+    };
+    //==========================
+    // Root Scope Event Handling
+    //==========================
     $rootScope.$on('$routeChangeStart', function (event, next, current) {
+        //Reroute if restricted and not logged in
         if (next.access.restricted && !AuthenticationService.isLoggedIn) {
             $location.path('/login');
+        } else {
+            //Update active for navbar
+            /* $rootScope.pageStatus[current.templateUrl].active = false; */
+            $rootScope.pageStatus[next.templateUrl].class = 'active';
         }
+        
     })
 })
